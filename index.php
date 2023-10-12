a3ff19dfb5157a56e5aa37be37b31003607d7eea
tantyotyo <tantyotyo29@gmail.com>
2023-10-12 10:31:00 +0700

    menambahkan paragraf pada index

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 </head>
 
 <body>
-
+    <h1>judul</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum non animi ut, tempore asperiores natus quo dicta corporis sed atque itaque! Dolor, omnis! Amet, adipisci non quia ipsum possimus reiciendis!</p>
 </body>
 
 </html>
